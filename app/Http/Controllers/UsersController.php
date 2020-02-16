e4be2267f3c538232d714920a7bc17d5229db62d
zeyuan <1973547260@qq.com>
2020-02-16 14:30:32 +0000

    用户注册页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-//用户控制器
 class UsersController extends Controller
 {
     public function create()
     {
-    	return view('users.create'); 
+        return view('users.create');
     }
 }
