3d7be33f1f72fe0f340441807cd04af352ddbf01
zeyuan <1973547260@qq.com>
2020-02-17 11:49:52 +0000

    Move user model to models folder

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,17 +6,10 @@
   </head>
 
   <body>
-
     @include('layouts._header')
 
     <div class="container">
       <div class="offset-md-1 col-md-10">
-
-
-   @include('layouts._header')
-
-    <div class="container">
-     <div class="offset-md-1 col-md-10">
         @yield('content')
         @include('layouts._footer')
       </div>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-
   <div class="jumbotron">
-
- <div class="jumbotron">
-
     <h1>Hello Laravel</h1>
     <p class="lead">
       你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
