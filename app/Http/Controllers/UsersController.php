6ed85e62375e7dcfaea66cdfbfe190ab237560ec
zeyuan <1973547260@qq.com>
2020-02-18 13:37:11 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,6 +23,7 @@ class UsersController extends Controller
     }
 
     //注册
+    //$request 用来接收用户输入的数据
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -30,7 +31,19 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' =>'required|confirmed|min:6'
         ]);
-        return;
+        //保存用户
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            ]);
+        // 我们可以使用 session() 方法来访问会话实例。而当我们想存入一条缓存的数据，让它只在下一次的请求内有效时，则可以使用 flash 方法。flash 方法接收两个参数，第一个为会话的键，第二个为会话的值，我们可以通过下面这行代码的为会话赋值。
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+        // 用户模型 User::create() 创建成功后会返回一个用户对象，并包含新注册用户的所有信息。我们将新注册用户的所有信息赋值给变量 $user，并通过路由跳转来进行数据绑定。
+        return redirect()->route('users.show',[$user]);
     }
+    
+    
     
 }
