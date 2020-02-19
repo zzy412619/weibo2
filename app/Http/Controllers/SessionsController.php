6a7a28d3f286a22ebd49d90ce3f61dca6350eddf
zeyuan <1973547260@qq.com>
2020-02-19 11:03:34 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -34,5 +34,12 @@ class SessionsController extends Controller
     		return redirect()->back()->withInput();
     	}
     }
+    // 用户退出
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出!');
+        return redirect('login');
+    }
 
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container ">
     <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
       @if (Auth::check())
         <li class="nav-item"><a class="nav-link" href="#">用户列表</a></li>
